0ce3535c91d18c30f77ffe78a5e7267b7322ebf3
walterdyna <alexdyna@gmail.com>
2024-12-02 19:56:49 -0300

    phpnovo

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,7 +18,7 @@
 			<div style="clear:both;"></div>
 		</div>
 
-		<script type="text/javascript" src="js/jquery.js"></script>
-		<script type="text/javascript" src="js/chess.js"></script>
+		<script type="text/javascript" src="./js/jquery.js"></script>
+		<script type="text/javascript" src="./js/chess.js"></script>
 	</body>
 </html>
